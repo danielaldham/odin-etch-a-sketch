2939697595f9dee652b4f9687864e385e76c4e81
Danny Aldham <daniel.aldham@gmail.com>
2023-11-01 22:24:49 +0100

    adjustable grid size

--- etch.js
+++ etch.js
@@ -1,21 +1,32 @@
+let slider = document.querySelector('#myRange');
+let output = document.querySelector("#size");
 const grid = document.querySelector('.grid');
 
-for (let i = 1; i <= 16; i++) {
-    for (let j = 1; j <= 16; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('square');
-        grid.appendChild(cell);
+output.innerHTML = slider.value; // Display the default slider value
+
+// Function to create a new grid
+function createGrid(size) {
+    grid.innerHTML = ''; // Clear the existing grid
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
+    for (let i = 1; i <= size * size; i++) { // Create size * size cells
+      const cell = document.createElement('div');
+      cell.classList.add('square');
+      // Attach the event listener to each cell as it's created
+      cell.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "black";
+      });
+      grid.appendChild(cell);
     }
-}
+  }
 
-// Select all grid cells
-const cells = document.querySelectorAll('.square');
+// Initial grid creation
+createGrid(slider.value);
 
-// Loop through each cell and attach the event listener
-cells.forEach(cell => {
-  cell.addEventListener("mouseenter", (event) => {
-    // Highlight the mouseenter target
-    event.target.style.backgroundColor = "black";
+// Update the grid whenever the slider value changes
+slider.oninput = function() {
+  output.innerHTML = this.value; // Update the displayed value
+  createGrid(this.value); // Create a new grid based on the slider's value
+};
 
-  });
-});
